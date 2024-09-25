05de20392000ccff1133184a373ab4a248a9d8db
Mohammed Bouali <Med.bouali96@hotmail.com>
2024-09-25 23:41:23 +0200

    fixed-testcontainers

--- user-service/src/test/java/com/individual_s7/user_service/TestUserServiceApplication.java
+++ user-service/src/test/java/com/individual_s7/user_service/TestUserServiceApplication.java
@@ -1,11 +1,11 @@
-package com.individual_s7.user_service;
-
-import org.springframework.boot.SpringApplication;
-
-public class TestUserServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.from(UserServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
-
-}
+//package com.individual_s7.user_service;
+//
+//import org.springframework.boot.SpringApplication;
+//
+//public class TestUserServiceApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.from(UserServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+//	}
+//
+//}
