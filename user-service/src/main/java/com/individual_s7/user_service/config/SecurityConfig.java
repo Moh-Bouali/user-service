e67e2306911eabdfa244349618d68c13286edac4
Mohammed Bouali <Med.bouali96@hotmail.com>
2024-12-06 11:29:37 +0100

    removed-security

--- user-service/src/main/java/com/individual_s7/user_service/config/SecurityConfig.java
+++ user-service/src/main/java/com/individual_s7/user_service/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -31,7 +32,8 @@ public class SecurityConfig {
 
         // Define authorization rules
         http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(antMatcher("/api/user/register")).permitAll()  // Allow unauthenticated access to /register
+                //.requestMatchers(antMatcher("/api/user/register")).permitAll()
+                .requestMatchers(antMatcher("/api/user/**")).permitAll()// Allow unauthenticated access to /register
                 .anyRequest().authenticated()  // All other requests require authentication
         );
 
